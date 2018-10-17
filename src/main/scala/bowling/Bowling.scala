8b675c5d0f89228ccb08c904f4870adc0f229e69
aminbaz <amin.bazaz@etu.umontpellier.fr>
2018-10-17 21:27:25 +0200

    add comment

--- src/main/scala/bowling/Bowling.scala
+++ src/main/scala/bowling/Bowling.scala
@@ -9,7 +9,16 @@ object Bowling extends App{
 }
 
 case class Game(frames : List[Frame]){
+  /**
+    * Take a list of Frame and edit them with the bonus when it's a strike or a spare
+    * @param frames List of 10 frames
+    */
   def updateFramesWithBonus(frames : List[Frame]) : List[Frame] = {
+    /**
+      * Take the score of the next roll to edit the score when its a spares
+      * @param index to find the frame which contains a spares bonus
+      * @return
+      */
     def getScoreOfNextRoll(index : Int) : Int = {
       if(index < frames.size){
         frames(index+1).firstRoll
@@ -17,6 +26,12 @@ case class Game(frames : List[Frame]){
         0
       }
     }
+
+    /**
+      * Get the score of the 2 next rolls to add it when its a strikes
+      * @param index to find the frame which contains a spares bonus
+      * @return
+      */
     def getScoreOf2NextRoll(index : Int) : Int = {
       if (index < frames.size - 1) {
         if (frames(index + 1).bonus == "strikes") {
@@ -38,6 +53,10 @@ case class Game(frames : List[Frame]){
       }
     })
   }
+
+  /**
+    * This Variable will contains the edited list of frame with the bonus
+    */
   val scoreFrames = updateFramesWithBonus(frames.map(x => {
     if(x.firstRoll == 10){
       x.copy(scoreFrame=x.firstRoll,bonus = "strikes")
@@ -48,6 +67,11 @@ case class Game(frames : List[Frame]){
     }
   }))
 
+  /**
+    * Compute the sum of each score frame
+    * @param frames List of 10 frames
+    * @param add the result in each iteration
+    */
   def sumResultFrames(frames : List[Frame], sum : Int): Int ={
     if(frames.isEmpty){
       sum
